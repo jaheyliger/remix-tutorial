1a9d5588fdda5e0568231d6c1ee4c61ed91feacb
jaheyliger <32755639+jaheyliger@users.noreply.github.com>
2023-10-30 23:03:22 -0400

    Added tailwind

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,12 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-  ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
-  // serverBuildPath: "build/index.js",
+	ignoredRouteFiles: ['**/.*'],
+	future: {
+		unstable_someFeature: true
+	},
+	tailwind: true
+	// appDirectory: "app",
+	// assetsBuildDirectory: "public/build",
+	// publicPath: "/build/",
+	// serverBuildPath: "build/index.js",
 };
